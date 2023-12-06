f01853f6b3b89713a153e0585c869b8f9d3b1eb7
Kilian <kilian.kugge@tuni.fi>
2023-12-06 13:35:32 +0200

    Day 3 second star solved

--- src/day03.py
+++ src/day03.py
@@ -27,8 +27,10 @@ def get_affected(start, end, y, lim_y, lim_x):
     for x in range(start, end+1):
         affected += [(y+1, x), (y-1, x)]
 
-    affected = filter(lambda coord: 0 <= coord[0] < lim_y and 0 <= coord[1] < lim_x, affected)
-    return list(affected)
+    affected = list(filter(lambda coord: 0 <= coord[0] < lim_y and 0 <= coord[1] < lim_x, affected))
+
+    # Remove possible duplicates
+    return list(dict.fromkeys(affected))
 
 
 def find_parts(numbers, lines):
@@ -63,11 +65,44 @@ def get_numbers(lines):
     return all_numbers
 
 
+def key_from_coord(y, x):
+    return f"{y}-{x}"
+
+
+def find_connected(numbers, lines):
+    gears_and_parts = {}
+    for number_info in numbers:
+        num, line_y, s, e = number_info.values()
+        adjacent = get_affected(s, e, line_y, len(lines), len(lines[0])-1)
+
+        for (y, x) in adjacent:
+            if lines[y][x] == '*':
+
+                k = key_from_coord(y, x)
+                if gears_and_parts.get(k) is None:
+                    gears_and_parts[k] = []
+                gears_and_parts[k].append(num)
+
+    connected_parts = []
+    for gear, part_nums in gears_and_parts.items():
+        if len(part_nums) == 2:
+            connected_parts.append((part_nums[0], part_nums[1]))
+
+    return connected_parts
+
+
 def main():
     lines = read_file_lines('../puzzleinputs/input03.txt')
     all_numbers = get_numbers(lines)
+
+    # Star 1
     parts = find_parts(all_numbers, lines)
     print(sum(parts))
+
+    # Star 2
+    connected_parts = find_connected(all_numbers, lines)
+    connected_multiplied = [p1 * p2 for (p1, p2) in connected_parts]
+    print(sum(connected_multiplied))
 
 
 if __name__ == '__main__':
