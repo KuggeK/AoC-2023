11b5e4fc7ee60114236535a3d448ff3078e423b1
Kilian <kilian.kugge@tuni.fi>
2023-12-12 16:52:41 +0200

    Modify reading file lines

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,3 @@
 def read_file_lines(filename):
-    file = open(filename, 'r')
-    lines = file.readlines()
-    file.close()
-    return lines
+    with open(filename) as file:
+        return file.read().splitlines()
